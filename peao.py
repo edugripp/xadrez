e5203929d9dfe2f1709fded0016d77d71d3b1ebe
Andrei dos Santos <andrei.santos@outsmartdigital.com.br>
2021-05-21 12:44:07 -0300

    parenthesis on super()

--- peao.py
+++ peao.py
@@ -8,3 +8,14 @@ class Peao(peca.Peca):
             posicao_x = 6
         posicao_y = ordem
         super().__init__(posicao_x, posicao_y, cor)
+
+    def possibleAvailablePositions(self):
+        direction_x = -1 if self.cor_peca == common.COR_BRANCA else 1
+        possible_available_positions = [[self.posicao_x + direction_x, self.posicao_y]]
+        if self.posicao_y > 0:
+            possible_available_positions.append([self.posicao_x + direction_x, self.posicao_y - 1])
+        if self.posicao_y < 7:
+            possible_available_positions.append([self.posicao_x + direction_x, self.posicao_y + 1])
+        if (self.posicao_x == 1 or self.posicao_x == 6) and 0 <= self.posicao_x + 2 * direction_x <= 7:
+            possible_available_positions.append([self.posicao_x + 2 * direction_x, self.posicao_y])
+        return possible_available_positions
--- torre.py
+++ torre.py
@@ -1,5 +1,6 @@
 import peca
 import common
+import tabuleiro
 
 class Torre(peca.Peca):
     def __init__(self, cor, ordem):
@@ -10,3 +11,21 @@ class Torre(peca.Peca):
         if ordem == 0:
                 posicao_y = 0
         super().__init__(posicao_x, posicao_y, cor)
+    
+    def possibleAvailablePositions(self):
+        possible_available_positions = []
+        for direction_x in [-1, 1]:
+            posicao_x = self.posicao_x + direction_x
+            while (not TABULEIRO.temPeca(posicao_x, self.posicao_y)) and 0 <= posicao_x <= 7:
+                possible_available_positions.append([posicao_x + direction_x, self.posicao_y])
+                posicao_x += direction_x
+            if 0 <= posicao_x <= 7 and TABULEIRO.temPeca(posicao_x, self.posicao_y) == self.cor_peca:
+                possible_available_positions.pop()
+        for direction_y in [-1, 1]:
+            posicao_y = self.posicao_y + direction_y
+            while (not TABULEIRO.temPeca(self.posicao_x, posicao_y)) and 0 <= posicao_y <= 7:
+                possible_available_positions.append([self.posicao_x, posicao_y + direction_y])
+                posicao_y += direction_y
+            if 0 <= posicao_y <= 7 and TABULEIRO.temPeca(self.posicao_x, posicao_y) == self.cor_peca:
+                possible_available_positions.pop()
+        return possible_available_positions
